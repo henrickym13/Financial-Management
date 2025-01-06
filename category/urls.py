fa66d7950871fddb9df62bd1d87788f3dd2c8cc5
unknown <henrique.so.ad@hotmail.com>
2025-01-06 18:29:12 -0300

    update category app

--- category/urls.py
+++ category/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('category/', views.categories, name='category'),
+    path('category/', views.category_list, name='category_list'),
+    path('category/add/', views.category_add, name='category_add'),
+    path('category/edit/<int:pk>/', views.category_edit, name='category_edit'),
+    path('category/delete/<int:pk>/', views.category_delete, name='category_delete')
 ]
--- category/views.py
+++ category/views.py
@@ -1,8 +1,52 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
+from .models import Category
+from .forms import CategoryForm
+from django.contrib import messages
 
 
 # Create your views here.
 @login_required
-def categories(request):
-    return render(request, 'category_list.html')
+def category_list(request):
+    categories = Category.objects.filter(user=request.user)
+    return render(request, 'category_list.html', {'categories': categories})
+
+
+@login_required
+def category_add(request):
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+        if form.is_valid():
+            category = form.save(commit=False)
+            category.user = request.user
+            category.save()
+            messages.success(request, 'Categoria adicionada com sucesso!')
+            return redirect('category_list')
+    else:
+        form = CategoryForm()
+    return render(request, 'category_add.html', {'form': form})
+
+
+@login_required
+def category_edit(request, pk):
+    category = get_object_or_404(Category, pk=pk, user=request.user)
+    if request.method == 'POST':
+        form = CategoryForm(request.POST, instance=category)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Categoria atualizada com sucesso!')
+            return redirect('category_list')
+    else:
+        form = CategoryForm(instance=category)
+    return render(request, 'category_edit.html', {'form': form})
+
+
+
+@login_required
+def category_delete(request, pk):
+    category = get_object_or_404(Category, pk=pk, user=request.user)
+    if request.method == 'POST':
+        category.delete()
+        messages.success(request, 'Categoria excluída com sucesso!')
+        return redirect('category_list')
+    return render(request, 'category_delete.html', {'category': category})
